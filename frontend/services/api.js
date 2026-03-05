013adae31d46890dec0108cd947b9e2c0947ebd4
Esteban V <esteban@envc.cloud>
2026-03-05 04:42:33 +0300

    feat: add base fetch wrapper and export API services

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,1 +1,8 @@
-// Base fetch wrapper
+export { ApiError, request, buildUrl } from './http.js';
+export { bankAccounts } from './bankAccounts.js';
+export { categories } from './categories.js';
+export { movements } from './movements.js';
+export { moneyTransfers } from './moneyTransfers.js';
+export { repetitiveMovements } from './repetitiveMovements.js';
+export { subCategories } from './subCategories.js';
+export { fxRates } from './fxRates.js';
